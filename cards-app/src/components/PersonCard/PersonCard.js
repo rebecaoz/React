c614389f54e962d44d7c710d817fbc3d1a9f05ac
Rebeca <lrpmqrp@gmail.com>
2024-03-12 02:00:45 -0300

    change value with setState

--- cards-app/src/components/PersonCard/PersonCard.js
+++ cards-app/src/components/PersonCard/PersonCard.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import './PersonCard.css';
 
-class PersonCard extends React.Component{
+class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            age: parseInt(props.age)
+        };
+    }
+    addAge=()=>{
+        this.setState((initialValue)=>({age: (initialValue.age+1) }))
+    }
+
     render(){
         const{firstName, lastName, age, hairColor}= this.props;
         return(
             <div className='card'>
-                <h1>{lastName}, {firstName} </h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor} </p>
+                <div className='data'>
+                    <h1>{lastName}, {firstName} </h1>
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {hairColor} </p>
+                </div>
+                
+                <button onClick={this.addAge}>Birthday Button for {firstName} {lastName} </button>
             </div>
         );
     }
